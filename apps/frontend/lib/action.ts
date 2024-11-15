3493366630b19dd79b501899c25f51f932b0bb57
Chaitanya Kadu <chaitanyakadu03@gmail.com>
2024-11-15 23:15:33 +0530

    Login redirect issue resolved

--- apps/frontend/lib/action.ts
+++ apps/frontend/lib/action.ts
@@ -3,7 +3,7 @@
 import { signIn, signOut } from "../auth"
 
 export async function SignIn() {
-  return await signIn('google', { redirectTo: "/api/auth/dashboard" })
+  return await signIn('google', { callbackUrl: "/api/auth/dashboard" })
 }
 
 export async function SignOut() {
