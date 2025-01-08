0f1e1b5c18305bdb0ced5070b438d2751afbd5cd
Chaitanya Kadu <chaitanyakadu03@gmail.com>
2025-01-08 15:28:02 +0530

    1.0.3v

--- apps/frontend/auth.ts
+++ apps/frontend/auth.ts
@@ -2,8 +2,14 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
 export const { handlers, auth, signIn, signOut }: any = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
   pages: {
     signIn: "/api/auth/signin",
-  }
+  },
+  secret: process.env.AUTH_SECRET,
 })
